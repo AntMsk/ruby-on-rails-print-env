3d145779a30f39129cde94d7be2716084ea5c6f1
Dmitrij Chest <antpslv@gmail.com>
2012-07-24 09:49:20 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LetterPrint::Application.config.secret_token = 'f345853ff4bbf9536656e17cf65261b0050328bd919718197d618e7b9f192c7588d50d29466de13720db4531e2c7340a54fc6037467097e6e4f6b484bbff9e1d'
